1ad5dafbeb7dd9720e29828712e4f35dd275a476
Akshat-paul <akshatpaul2006@gmail.com>
2025-03-11 17:07:15 +0530

    commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,71 @@
 
-var a = prompt("What is your name");
-document.write("User is ");
+// var a = prompt("What is your name");
+// console.log(a);
 
+// let age = 18;
+// if(age>=18)
+// {
+//     console.log("You are an adult");
+// }
+// else{
+//     console.log("You are not an adult");
+
+// }
+
+
+
+// let score = 90;
+// if(score >= 90)
+// {
+//     console.log("Grade A");
+
+// }
+// else if (score>=80)
+// {
+//     console.log("Grade B");
+// }
+// else if (score >=70)
+// {
+//     console.log("Grade C");
+// }
+// else if (score>=60)
+// {
+//     console.log("Grade D");
+// }
+// else
+// {
+//     console.log("Fail");
+
+// }
+
+
+
+
+let day = "Monday";
+switch(day)
+{
+    case "Monday":
+        console.log("Today is Monday");
+        break;
+    case "Tuesday":
+        console.log("Today is Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Today is Wednesday");
+        break;
+    case "Thursday":
+        console.log("Today is Thursday");
+        break;
+    case "Friday":
+        console.log("Today  is Friday");
+        break;
+    case "Saturday":
+        console.log("Today is Saturday");
+        break;
+    case "Sunday":
+        console.log("Today is Sunday");
+        break;
+    default:
+        console.log("Invalid entry of day");
+        break;
+}
